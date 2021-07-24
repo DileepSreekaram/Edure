d3b1b0cecd49c20cf452bdc7135f2337be4fa143
DileepSreekaram <79400338+DileepSreekaram@users.noreply.github.com>
2021-07-24 13:40:53 +0530

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -1,1 +1,65 @@
+import docx
+import shutil
+import re
+import os
+
+class Operation:
+    @classmethod
+    def segregate(cls, src:str)->str:
+        """
+        This method will segregate all the files at the specific source location into the folders, which the program creates itself based on the name of the file.
+        :param src: str
+        :return: str
+        """
+        meta_dict = {}
+        patt_lst = []
+
+        for file in os.listdir(src):
+
+            # Rename the files without '_'
+            if not ("_" in file) and not (file.endswith(".py")):
+                # print(file)
+                name = re.findall(r"^[A-Za-z][A-Za-z0-9_]+[^_^.]", file)[0] # Assignment1
+                ext = re.findall(r"[.].+", file)[0] #.docx
+                # print(name,ext)
+                rename_file = f"Programming_" + name + " (1)" + ext  # Programming_Assignment1 (1).docx
+                os.rename(src=file, dst=rename_file)
+
+        # Rename the duplicate files
+        # # Replacing () with Advanced
+        for file in os.listdir(src):
+            if not os.path.isdir(file):
+
+                if re.search(r"\(\d\)", file):
+                    rename_file = re.sub(r"\s\(\d\)", "(Advanced)", file)
+                    os.rename(src=file, dst=rename_file)
+                else:
+                    rename_file = file
+
+                # Create the respective folders
+                cat = re.findall(r"^[A-Za-z_\(\)]+[^0-9^.^_^\s]", rename_file)[0]
+
+
+                patt = re.findall(r"[A-Za-z]+[^_]", cat)[0]
+
+                # Creating the respective directories...
+                if not (rename_file.endswith(".py")):
+                    if re.search("\(Advanced\)", rename_file):
+                        cat += "_Advanced"  # Assignment_Advanced
+                        patt += "_A"
+                        meta_dict[patt] = cat
+                    else:
+                        cat += "_Basic"
+                        patt += "_B"
+                        meta_dict[patt] = cat
+
+
+                    if patt not in patt_lst:
+                        patt_lst.append(patt)
+                        os.mkdir(meta_dict[patt])
+
+                # Moving the files to respective folders
+                if not rename_file.endswith(".py"):
+                    shutil.move(src=rename_file, dst=f".\\{meta_dict[patt]}")
+    print("All files segregated successfully")
 
